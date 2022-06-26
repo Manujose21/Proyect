0f3e2c9e05b52f82b3dfcc2f8142e4cf8e662e28
Manuel <m4nufra@gmail.com>
2022-06-25 20:26:47 -0400

    login and register

---
+++ Views/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/styles/style.css">
+    <title>Iniciar Sesion</title>
+</head>
+<body class="bg-dark">
+    <div class="d-flex justify-content-center mt-5">
+        <div class="card w-50 p-4 bg-ligth">
+           
+            <h1 class="display-5 text-center mb-3">Iniciar Sesion</h1>
+            
+            <form method="post">
+                <input type="text" class="form-control mb-3" name="user" placeholder="Usuario">
+                <input type="password" class="form-control mb-3" name="pass" placeholder="Contraseña">
+                <div class="d-flex justify-content-center">
+                    <input type="submit" class="btn btn-dark w-50" value="Iniciar">
+                </div>
+            </form>
+            <a href="register.php" class="link mt-3">Registrarse</a>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ database.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS loan_system;
+
+CREATE DATABASE loan_system;
+
+use loan_system;
+
